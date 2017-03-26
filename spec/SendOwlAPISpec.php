ac52d5665090eada19ccbd20289508023eda7b9c
Andreas Nurbo <andreas.nurbo@gmail.com>
2017-03-26 17:12:12 +0200

    add per page and page get_products arguments

--- spec/SendOwlAPISpec.php
+++ spec/SendOwlAPISpec.php
@@ -23,6 +23,14 @@ class SendOwlAPISpec extends ObjectBehavior {
 		$this->get_products()->shouldReturn( json_decode( $transport->body, true ) );
 	}
 
+	public function it_should_retrieve_products_per_page_and_page() {
+		$transport       = new MockTransport();
+		$transport->code = '200';
+		$transport->body = file_get_contents( __DIR__ . '/data/products.json' );
+		$this->beConstructedWith( 'key', 'secret', [ 'transport' => $transport ] );
+		$this->get_products(5,1)->shouldReturn( json_decode( $transport->body, true ) );
+	}
+
 	public function it_should_retrieve_product() {
 		$transport       = new MockTransport();
 		$transport->code = '200';
--- src/SendOwlAPI.php
+++ src/SendOwlAPI.php
@@ -30,9 +30,9 @@ class SendOwlAPI {
 	 * @param array|null $options
 	 */
 	public function __construct( string $key, string $secret, array $options = null ) {
-		$this->key     = $key;
-		$this->secret  = $secret;
-		$this->options = $options;
+		$this->key             = $key;
+		$this->secret          = $secret;
+		$this->options         = $options;
 		$this->options['auth'] = array( $this->get_key(), $this->get_secret() );
 	}
 
@@ -53,12 +53,17 @@ class SendOwlAPI {
 	/**
 	 * Retrieves products
 	 *
+	 * @param int $per_page Default is 10
+	 * @param int $page Default is 1
+	 *
 	 * @return array
 	 * @throws Exception
 	 */
-	public function get_products() {
-		$headers = array('Accept' => 'application/json');
-		$response = Requests::get( $this->url .'/products', $headers, $this->options );
+	public function get_products( int $per_page = 10, int $page = 1 ) {
+		$headers     = [ 'Accept' => 'application/json' ];
+		$query_array = [ 'per_page' => $per_page > 0 ?: 10, 'page' => $page >= 1 ?: 1 ];
+		$query       = http_build_query( $query_array );
+		$response    = Requests::get( $this->url . '/products/?' . $query, $headers, $this->options );
 		if ( $response->success ) {
 			return json_decode( $response->body, true );
 		}
@@ -71,13 +76,12 @@ class SendOwlAPI {
 	 * @return array
 	 * @throws Exception
 	 */
-    public function get_product(int $product_id)
-    {
-	    $headers = array('Accept' => 'application/json');
-	    $response = Requests::get( $this->url .'/products/' . $product_id, $headers, $this->options );
-	    if ( $response->success ) {
-		    return json_decode( $response->body, true );
-	    }
-	    throw new Exception( $response->body, $response->status_code );
-    }
+	public function get_product( int $product_id ) {
+		$headers  = [ 'Accept' => 'application/json' ];
+		$response = Requests::get( $this->url . '/products/' . $product_id, $headers, $this->options );
+		if ( $response->success ) {
+			return json_decode( $response->body, true );
+		}
+		throw new Exception( $response->body, $response->status_code );
+	}
 }
